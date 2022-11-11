249d0b3cfe8955acb093dde9458dc908e99a86d5
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-11 21:24:58 +0300

    task2_alt

---
+++ task2_alt/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Week days!");
+string[] Days = {"Minday","Tuesday","Wensday","Thursday","Friday","Saturday","Sunday"};
+int index = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Today is {Days[index-1]}");
