d5e8bb2f7641ac2bdcc150e5daf1108421835006
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-11 20:56:43 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A == B*B) 
+{
+    Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("НЕТ");
+}
