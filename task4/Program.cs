da7eca375253a4a32252977bbf032053611fa630
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-11 22:10:32 +0300

    task4_1

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,7 +2,7 @@
 int a = Convert.ToInt32(Console.ReadLine());
 if (a > 100 && a < 1000)
 {
-    Console.WriteLine(a%10);
+    Console.WriteLine(a % 10);
 }
 else
     Console.WriteLine("Число не трехзначное");
