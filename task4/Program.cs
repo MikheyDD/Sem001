a76227d0d1c5200e45d7a7fdda04d688ebfd2e22
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-11 22:08:32 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ВВедите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 100 && a < 1000)
+{
+    Console.WriteLine(a%10);
+}
+else
+    Console.WriteLine("Число не трехзначное");
